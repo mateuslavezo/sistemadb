343bf3179cf4e30aa3b3300268dc96af88b26181
mateuslavezo <43421994+mateuslavezo@users.noreply.github.com>
2019-10-22 17:26:04 -0300

    sistemadb-atividade-22-10

---
+++ sistemaweb/sistemaweb/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sistemaweb.Models
+{
+    public partial class Produtos
+    {
+        public int ProdCodigo { get; set; }
+        public string ProdNome { get; set; }
+        public decimal ProdPreco { get; set; }
+        public int ProdQtde { get; set; }
+    }
+}
---
+++ sistemaweb/sistemaweb/Models/sistemadbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace sistemaweb.Models
+{
+    public partial class sistemadbContext : DbContext
+    {
+        public sistemadbContext()
+        {
+        }
+
+        public sistemadbContext(DbContextOptions<sistemadbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCodigo);
+
+                entity.Property(e => e.ProdCodigo).HasColumnName("Prod_Codigo");
+
+                entity.Property(e => e.ProdNome)
+                    .IsRequired()
+                    .HasColumnName("Prod_Nome")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProdQtde).HasColumnName("Prod_Qtde");
+            });
+        }
+    }
+}
